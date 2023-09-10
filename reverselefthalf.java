1c06bb88e25a1d0f774a51798b38dc2ccfb05713
Priya07Singh <122694924+Priya07Singh@users.noreply.github.com>
2023-09-10 21:56:33 +0530

    Add files via upload

---
+++ reverselefthalf.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class reverselefthalf {
+			public static void printPattern(int n)
+			{
+				int i, j;
+				for (i = n; i >= 1; i--) {
+					for (j = 1; j <= i; j++) {
+						System.out.print("*");
+					}
+					System.out.println();
+				}
+			}
+			public static void main(String args[])
+			{
+				int n = 6;
+				printPattern(n);
+			}
+		
+
+	}
+
+
